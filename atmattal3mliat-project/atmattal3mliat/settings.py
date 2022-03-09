47077822383c731203206ebdb770bebbc0bfb455
suhaib-rabba <81135889+suhaib-rabba@users.noreply.github.com>
2022-03-09 11:47:04 -0800

    الربط الاولي

--- atmattal3mliat-project/atmattal3mliat/settings.py
+++ atmattal3mliat-project/atmattal3mliat/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'atmattal3mliat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
