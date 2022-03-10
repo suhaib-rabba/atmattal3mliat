ce9d2cffa54a58e93c59bc5198cdbe1eb561dfa9
suhaib-rabba <81135889+suhaib-rabba@users.noreply.github.com>
2022-03-10 10:04:39 -0800

    على بركة الله

--- atmattal3mliat-project/atmattal3mliat/urls.py
+++ atmattal3mliat-project/atmattal3mliat/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from apps import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
-]
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
