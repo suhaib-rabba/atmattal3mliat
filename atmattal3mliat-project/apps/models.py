90fee2fa5159c3d659fff25c4f2629882ba423bf
suhaib-rabba <81135889+suhaib-rabba@users.noreply.github.com>
2022-05-12 03:36:10 -0700

    commite

--- atmattal3mliat-project/apps/models.py
+++ atmattal3mliat-project/apps/models.py
@@ -98,7 +98,7 @@ class monthlyReportModel(models.Model):
              default=datetime(2022, 11, 4, 0, 5, 23))
     area = models.CharField(max_length=255)
     work = models.TextField()
-    image = models.FileField(upload_to='monthlyReport/', blank=True)
+    image = models.ImageField(upload_to='monthlyReport/', blank=True)
 
     def __str__(self):
         return self.department
